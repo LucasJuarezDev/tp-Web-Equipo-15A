023576d38b0de8e6e1673e591914e15b6bf907c0
Axel <axelpereyra_la12@hotmail.com>
2025-09-29 18:57:34 -0300

    clase voucher,categoria,articulo

---
+++ TPWeb_Equipo-15/Dominio/Articulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Articulo
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+      //  public Marca TipoMarca { get; set; }  falta crear esto
+     //   public Categoria TipoCategoria { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
